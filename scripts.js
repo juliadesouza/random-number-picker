af1611b5da55a1ac820748ea0884bb6d7cdad583
Julia Souza <juliasouza@Julias-MacBook-Pro.local>
2025-01-28 14:41:03 -0300

    feat: added noRepeat validation

--- scripts.js
+++ scripts.js
@@ -2,33 +2,44 @@ const form = document.querySelector("form");
 const quantity = document.getElementById("quantity");
 const start = document.getElementById("start");
 const end = document.getElementById("end");
+const toggle = document.getElementById("switch");
 
 form.onsubmit = (e) => {
   e.preventDefault();
-  getRandomNumbers();
+  const numbers = getRandomNumbers();
 };
 
-// sanitizar entradas pra nao aceitar letras
-// sanitizar entradas pra quantity nao ser negativa
 function getRandomNumbers() {
   try {
+    const noRepeat = toggle.checked;
     const min = Number(start.value);
     const max = Number(end.value);
 
     if (min >= max) {
-      alert(
+      return alert(
         "Ops! Certifique-se de que o número inicial seja menor que o número final."
       );
     }
 
+    if (noRepeat && max - min < quantity.value) {
+      return alert(
+        `Ops! Não é possível gerar ${quantity.value} números não repetidos neste intervalo.`
+      );
+    }
+
     const numbers = [];
-    for (let i = 0; i < quantity.value; i++) {
+
+    while (numbers.length < quantity.value) {
       const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+
+      if (noRepeat && numbers.includes(randomNumber)) {
+        continue;
+      }
 
       numbers.push(randomNumber);
     }
 
-    console.log(numbers.join("/"));
+    return numbers;
   } catch (error) {
     console.log(error);
   }
